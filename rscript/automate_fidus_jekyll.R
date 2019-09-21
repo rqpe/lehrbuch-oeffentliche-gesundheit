aa9ffc0fde5307893fa73eb0e66bd564013adba5
Jakob Schumacher <jakob.schumacher@web.de>
2019-09-21 23:48:08 +0200

    Title is capitalized

--- rscript/automate_fidus_jekyll.R
+++ rscript/automate_fidus_jekyll.R
@@ -1,36 +1,31 @@
 
-# This script automates the the process of passing html files into the jekyll system. Its done for the handbook for infectious diseases
+# This script automates the the process of passing html files 
+# into the jekyll system. Its done for the handbook for infectious diseases
 
 
 # This command lists all zip files 
 zipfiles <- list.files(".", pattern = ".zip$")
 
-# These commands remove all old files
-file.remove(list.files(".", pattern = ".html$"))
-file.remove(list.files(".", pattern = ".png$"))
-
-
+# loop through all zip files
 for (a in zipfiles) {
-# a <- zipfiles[2]
 
 # Getting the necessary information out of the zip-file name
 name_separted <- strsplit(a, "\\.")[[1]]
 name_separted <- strsplit(name_separted[1], "-")[[1]]
 name_separted_blanks <- paste(name_separted[2:length(name_separted)], collapse = " ")
+name_separted_blanks <- paste0(toupper(substr(name_separted_blanks, 1, 1)), substr(name_separted_blanks, 2, nchar(name_separted_blanks)))
 name_separted_underscore <- paste(name_separted[2:length(name_separted)], collapse = "_")
 newfilename <- paste0("../docs/",name_separted_underscore,".html")
 number <- strsplit(name_separted[1], split = "(?<=[a-zA-Z])\\s*(?=[0-9])", perl = TRUE)[[1]][[2]]
 number <- sub("^0+", "", number)
 
 
-
 # Unzip the file
 unzip(a)
 
 
-# Get rid of the annoying error 
+# Get rid of the annoying error of a missing newline
 write("\n", "document.html", append = T)
-
 
 # Remove html head and css from document.html
 document <- readLines("document.html") 
@@ -39,9 +34,10 @@ finish <- grep("</body", document) - 1
 document <- document[start:finish]
 # write(document[start:finish], file = "test.html")
 
-
+# remove the old file if it exists
 if(file.exists(newfilename)) file.remove(newfilename)
 
+# the new file is created line by line. This is where the jekyll front matter is included
 write("---", newfilename)
 write("layout: page", file = newfilename, append = T)
 write(paste("title:", name_separted_blanks), newfilename, append = T)
@@ -63,13 +59,4 @@ for (i in pics) {
 # Cleaning up
 file.remove(list.files(".", pattern = ".png$"))
 file.remove(list.files(".", pattern = ".woff$"))
-file.remove(list.files(".", pattern = ".html$"))
-file.remove(list.files(".", pattern = ".zip$"))
-
-
-
-
-
-
-
 
